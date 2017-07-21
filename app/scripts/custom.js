d473252a400dea3eab1e10236581d3301c36df7d
Jamil <jamil.abdallah@gmail.com>
2017-07-21 03:17:11 +0300

    homewoek

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -1,12 +1,11 @@
 jQuery(window).scroll(function() {
     var scroll = jQuery(window).scrollTop();
 
-
     if (scroll >= 190 && jQuery(window).width() > 767 ) {
-        jQuery(".topbar").addClass("scrolling");
+        jQuery(".navbar").addClass("scrolling");
 
     } else {
-        jQuery(".topbar").removeClass("scrolling");
+        jQuery(".navbar").removeClass("scrolling");
     }
 });
 
