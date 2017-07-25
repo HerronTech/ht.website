8b9fcd2fb0cbb4b19a0cf09ea212e3ba733a293b
Jamil <jamil.abdallah@gmail.com>
2017-07-25 16:45:56 +0300

    disruption - basic no style

--- sections/disruption/controller.js
+++ sections/disruption/controller.js
@@ -3,7 +3,7 @@ var digitalApp = app.components;
 digitalApp.controller('digitalPageCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
 
     var pageData = {
-    title: "We give you digital agility"
+    title: "Why digital agility"
     };
     $scope.$parent.$emit('refreshPageTitle', pageData);
 
