08daf8b041e93bd1dc2bf46d2021d731d101dc00
Jamil <jamil.abdallah@gmail.com>
2017-07-25 18:28:45 +0300

    platform - skeleton

--- app/navigation.js
+++ app/navigation.js
@@ -10,14 +10,33 @@ var navigation = [
 		'tplPath': 'sections/home/page.html',
 		'scripts': ['sections/home/controller.js']
 	},
-	{
-		'id': 'digital',
-		'label': 'digital Vortex',
-		'title': 'Herron Tech | Digital Vortex',
-		'url': '/digital',
-		'tplPath': 'sections/digital/page.html',
-		'scripts': ['sections/digital/controller.js']
-	}
+  {
+    'id': 'digital',
+    'label': 'A change is happening',
+    'title': 'Herron Tech | We give you digital agility',
+    'url': '/digital',
+    'tplPath': 'sections/digital/page.html',
+    'scripts': ['sections/digital/controller.js']
+  },
+  {
+    'id': 'disruption',
+    'label': 'The Disruption',
+    'title': 'Herron Tech | Why digital agility',
+    'url': '/disruption',
+    'tplPath': 'sections/disruption/page.html',
+    'scripts': ['sections/disruption/controller.js']
+  }
+  ,
+  {
+    'id': 'platform',
+    'label': 'Our Platform',
+    'title': 'Herron Tech | Our Platform',
+    'url': '/platform',
+    'tplPath': 'sections/platform/page.html',
+    'scripts': ['sections/platform/controller.js']
+  }
+
+
 ];
 
 var whitelistedDomain = ['localhost'];
--- sections/disruption/controller.js
+++ sections/disruption/controller.js
@@ -1,6 +1,6 @@
 "use strict";
-var digitalApp = app.components;
-digitalApp.controller('digitalPageCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
+var disruptionApp = app.components;
+disruptionApp.controller('digitalPageCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
 
     var pageData = {
     title: "Why digital agility"
