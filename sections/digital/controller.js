fd2cfd52c8640c4ee6772793b6e418d4c91025ca
mikehajj <mike.hajj@gmail.com>
2017-07-25 16:36:57 +0300

    moved script tag inside body on main index page.
    updated navigation title entry, updated route change success to invoke anchors when available.
    
    updated template to provide anchor using the correct angular synthax.

--- sections/digital/controller.js
+++ sections/digital/controller.js
@@ -3,7 +3,7 @@ var digitalApp = app.components;
 digitalApp.controller('digitalPageCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
 
     var pageData = {
-    title: "Digital Transformation"
+        title: "Digital Transformation"
     };
     $scope.$parent.$emit('refreshPageTitle', pageData);
 
