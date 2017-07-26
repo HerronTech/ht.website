44cd649f96e715c5e7ecaa806969ba2f07f2d95e
Jamil <jamil.abdallah@gmail.com>
2017-07-26 19:22:59 +0300

    patch images

--- sections/digital/controller.js
+++ sections/digital/controller.js
@@ -2,10 +2,11 @@
 var digitalApp = app.components;
 digitalApp.controller('digitalPageCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
 
-    var pageData = {
-    title: "We give you digital agility"
-    };
-    $scope.$parent.$emit('refreshPageTitle', pageData);
+  var pageData = {
+    title: "We give you digital agility",
+    menuClass: "solution"
+  };
+  $scope.$parent.$emit('refreshPageTitle', pageData);
 
 
 }]);
