a2d810e95ae176f7ec318e3465a6b35069eede53
antoinehage <me@antoinehage.com>
2018-02-02 14:03:13 -0500

    added interval for mycarousela and changed the controller name of home page agility

--- sections/homeagility/controller.js
+++ sections/homeagility/controller.js
@@ -1,6 +1,6 @@
 "use strict";
 var homeApp = app.components;
-homeApp.controller('homePageCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
+homeApp.controller('homePageAgileCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
 
     var pageData = {
         title: "We give you digital agility",
