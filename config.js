e69ecbcce5d4e1fc207fa26a613e59a5109154a2
mikehajj <mike.hajj@gmail.com>
2018-02-09 12:26:35 +0200

    added support for custom settings file and changed default tenant key to use public soajs value

--- config.js
+++ config.js
@@ -25,9 +25,16 @@ mydomain += ":" + mydomainport;
 
 //set the api domain
 var mydomainAPI = "cloud-api";
+if(customSettings && customSettings.api && customSettings.api !== ''){
+	mydomainAPI = customSettings.api;
+}
 
 //set the key
-var myKey = "507db56cc1fecfde8d4853334cdbb0f08181d7a09d20ef7d79ed0622dc2d47c477c7c10da077fad0b70fe9eedada19bda7ade58adccc8045ba779376062da9bd637d9a39f43072b8d8ab82fed474896154843fb57bffbb4088b43973c98c2e28";
+var myKey = "9b96ba56ce934ded56c3f21ac9bdaddc8ba4782b7753cf07576bfabcace8632eba1749ff1187239ef1f56dd74377aa1e5d0a1113de2ed18368af4b808ad245bc7da986e101caddb7b75992b14d6a866db884ea8aee5ab02786886ecf9f25e974";
+
+if(customSettings && customSettings.key && customSettings.key !== ''){
+	myKey = customSettings.key;
+}
 
 var whitelistedDomain = ['localhost', '127.0.0.1', mydomainAPI + '.' + mydomain];
 var apiConfiguration = {
