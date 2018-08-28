71e95bbbfafdfce89288be6acf35a77648f0b6ce
mikehajj <mike.hajj@gmail.com>
2018-08-28 18:49:50 +0300

    updated how url is composed

--- sections/home/controller.js
+++ sections/home/controller.js
@@ -47,9 +47,10 @@ homeApp.controller('homePageCtrl', ['$scope', '$http', '$timeout', '$modal', fun
 	$scope.sendContact = function(){
 		$scope.alerts.push({ 'type': 'warning', 'msg': "Your message is being sent, please wait ..." });
 		if($scope.contact.captcha){
+			let address = window.location.protocol + "://" + window.location.port + "api.herrontech.com";
 			$http({
 				method: 'POST',
-				url: 'https://api.herrontech.com/herrontech/contact',
+				url: address + '/herrontech/contact',
 				data: $scope.contact,
 				headers: { 'Content-Type': 'application/json', key: myKey }
 			}).success(function (data, status, headers, config) {
